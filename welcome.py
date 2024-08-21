ad51e2b44cba78ff0cf1a2b4338dfe99ad7ba5c4
BANAAG-KYLE <23-03663@g.batstate-u.edu.ph>
2024-08-21 11:36:55 +0800

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,10 @@
+def main():
+    
+    print("Welcome to CS 121: Advance Computer Programming!")
+    
+    name=input("Enter your name: ")
+    
+    print(f"This course will be fun, {name}!")
+    
+if __name__ == "__main__":
+    main()
