f6c298cc893cdbd74cec0eefe9f390443bfd61e3
BANAAG-KYLE <23-03663@g.batstate-u.edu.ph>
2024-09-11 10:41:04 +0800

    Delete Exercise Sets directory

--- Exercise Sets/Exercise Set 02/bitwise2.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Enter an integer: "))
-result = n & 30
-print(f"{n} AND 30 = {result}")
--- Exercise Sets/Exercise Set 02/typecasting.py
+++
@@ -1,5 +0,0 @@
-integer_input = int(input("Enter an integer: "))
-float_input = float(input("Enter a float: "))
-difference = float(integer_input) - float_input
-
-print("Difference:", "{:.2f}".format(difference))
